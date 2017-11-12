b5a1ac9b92daff52e10ee9cb24b94e1c6d4daf74
sqz777 <sqz777isme@gmail.com>
2017-11-13 00:04:19 +0800

    Failed First Test case

--- Codewars_EvilorOdious/UnitTest1.cs
+++ Codewars_EvilorOdious/UnitTest1.cs
@@ -4,11 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Codewars_EvilorOdious
 {
     [TestClass]
-    public class UnitTest1
+    public class EvilOrOdiousTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_0_Should_Be_ItsEvil()
         {
+            var actual = EvilOrOdious.Evil(0);
+            Assert.AreEqual("It's Evil!",actual);
+        }
+    }
+
+    public class EvilOrOdious
+    {
+        public static string Evil(int i)
+        {
+            throw new NotImplementedException();
         }
     }
 }
