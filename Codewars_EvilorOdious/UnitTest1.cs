f4a81ac32facdbfb22b136397678f0419ccfdddb
sqz777 <sqz777isme@gmail.com>
2017-11-13 00:12:12 +0800

    Pass 2nd But Failed 1st test case

--- Codewars_EvilorOdious/UnitTest1.cs
+++ Codewars_EvilorOdious/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Codewars_EvilorOdious
@@ -10,15 +11,22 @@ namespace Codewars_EvilorOdious
         public void Input_0_Should_Be_ItsEvil()
         {
             var actual = EvilOrOdious.Evil(0);
-            Assert.AreEqual("It's Evil!",actual);
+            Assert.AreEqual("It's Evil!", actual);
+        }
+
+        [TestMethod]
+        public void Input_1_Should_Be_ItsOdious()
+        {
+            var actual = EvilOrOdious.Evil(1);
+            Assert.AreEqual("It's Odious!", actual);
         }
     }
 
     public class EvilOrOdious
     {
-        public static string Evil(int i)
+        public static string Evil(int n)
         {
-            throw new NotImplementedException();
+            return Convert.ToString(n, 2).Count(x => x == 1) % 2 == 0 ? "It's Evil!": "It's Odious!";
         }
     }
 }
